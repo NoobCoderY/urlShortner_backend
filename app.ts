a0c5f410418d66ddcc29379e0ed91a0ed25ca4e7
Yash Diwaker <69832240+NoobCoderY@users.noreply.github.com>
2023-10-28 11:24:09 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ const app = express();
 
  app.use(cors({
     credentials: true,
-    origin:["http://localhost:3000","https://url-shortner-frontend-9fa2.vercel.app/","*"],
+    origin:["http://localhost:3000","https://url-shortner-frontend-9fa2-30u8cqm50-noobcodery.vercel.app/","*"],
   }))
   app.use(express.json());
   app.use(express.urlencoded({
